1468fcb793e96c16d00c2c2f6e00beadd157be12
Ray819038825 <u1537043@utah.edu>
2024-08-22 12:42:26 -0600

    Ray
    Start bottom

---
+++ Assets/Scripts/UI/Start Game.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Scene;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start(StartGame)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex;
+
+
+    }
